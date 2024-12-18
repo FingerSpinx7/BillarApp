7f02ac8cfe3f0bc31680c7f5479c1258f1c6b699
DavidSanchez14 <deivid14122004shz@gmail.com>
2024-12-17 23:06:52 -0600

    Funcionalidad de Detalles de Consulta

--- app/src/main/java/com/example/billarapp/domain/model.kt
+++ app/src/main/java/com/example/billarapp/domain/model.kt
@@ -1,6 +1,5 @@
 package com.example.billarapp.domain
 
-
 @kotlinx.serialization.Serializable
 data class ProductoModel(
     val id_producto: Int,
@@ -8,4 +7,21 @@ data class ProductoModel(
     val det_producto: String,
     val precio: Float,
     val Cantidad_Inv: Int?=0
+)
+
+@kotlinx.serialization.Serializable
+data class CuentaModel(
+    val id_billar: Int,
+    val id_cuenta: Int,
+    val Numero_mesa: Int,
+    val fecha_inicio: String,
+    val fecha_fin: String,
+    val cliente: String
+)
+
+@kotlinx.serialization.Serializable
+data class DetalleProductosConsumidosModel(
+    val descripcion: String,
+    val cantidad: Int,
+    val total: Double
 )
--- app/src/main/java/com/example/billarapp/domain/useCase.kt
+++ app/src/main/java/com/example/billarapp/domain/useCase.kt
@@ -8,10 +8,11 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.billarapp.data.network.supabaseBillar
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.postgrest
 import kotlinx.coroutines.launch
-
-
-
+import io.github.jan.supabase.postgrest.rpc
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
     @Composable
@@ -35,3 +36,41 @@ import kotlinx.coroutines.launch
         }
         return productos
     }
+
+suspend fun getCuentasFromDatabase(): List<CuentaModel> {
+    return try {
+        withContext(Dispatchers.IO) {
+            val data = supabaseBillar()
+                .from("cuenta")
+                .select()
+                .decodeList<CuentaModel>()
+            data
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Log.e("dbg", "Error: ${e.message}")
+        emptyList()
+    }
+}
+
+
+@Composable
+fun getDetalleProductosConsumidos(idCuenta: Int): SnapshotStateList<DetalleProductosConsumidosModel> {
+    val detalles = remember { mutableStateListOf<DetalleProductosConsumidosModel>() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            try {
+                val data = supabaseBillar()
+                    .postgrest.rpc("filtrar_productos", mapOf("id_filtro" to idCuenta)) // Llama correctamente a la RPC
+                    .decodeList<DetalleProductosConsumidosModel>()
+                detalles.addAll(data)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                println("Error: ${e.message}")
+            }
+        }
+    }
+    return detalles
+}
